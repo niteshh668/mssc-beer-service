235b2752291e5c2ca5c324b0e70f716343929c3d
nitesh <nitesh.h668@gmail.com>
2020-08-03 21:48:53 -0400

    closes #4 adding controller tests

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,11 +1,8 @@
 package guru.springframework.msscbeerservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MsscBeerServiceApplicationTests {
 
